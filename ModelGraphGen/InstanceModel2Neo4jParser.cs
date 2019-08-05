c66804a7c1816084ff8fc62a0cf4b88e23eb403c
Sebastian Esser@TUBVCMS-SES-3-W <sebastian.esser@tum.de>
2019-08-05 10:35:50 +0200

    BasicSetup

---
+++ ModelGraphGen/InstanceModel2Neo4jParser.cs
@@ -0,0 +1,46 @@
+﻿namespace TUMCMS_SE_ModelGraphGen
+{
+    public class InstanceModel2Neo4jParser
+    {
+        private string SourceLocation { get; set; }
+        private string TargetLocation { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public InstanceModel2Neo4jParser()
+        {
+        }
+
+        public InstanceModel2Neo4jParser(string sourceLocation)
+        {
+            SourceLocation = sourceLocation;
+        }
+
+
+        public void CreateNeo4jScript()
+        {
+            var sourceFile = SourceLocation;
+            var modelType = "IFC";
+            // switch format
+
+            switch (modelType)
+            {
+                case "IFC":
+
+                    break;
+
+                case "PlanPro":
+
+                    break;
+
+                case "RailML":
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
