ad517418d2f15efc739cbdcb858b901e7d81eecd
Sebastian Esser <sebastian.esser@tum.de>
2020-08-29 22:46:43 +0200

    tried to receive a socket message. some mess in py client

--- CM-client_pythonCMD/client.py
+++ CM-client_pythonCMD/client.py
@@ -3,34 +3,51 @@
 import socketio 
 import time
 
-# --- methods ---
+## --- methods ---
+#class Connector(socketio.Namespace): 
+#    sio = socketio.Client()
 
+#    def __init__(self): 
+#        sio.connect('http://localhost:3000')
+#        pass
+
+#    @sio.on('connect')
+#    def connect_handler():        
+#        print("I'm connected!")
+
+#    @sio.event
+#    def my_event(data):
+#    print('Received data: ', data)
+
+
+## --- script --- 
+#connector = Connector
+
+sio = socketio.Client(engineio_logger=True)
+start_timer = None
+
+
+def send_ping():
+    global start_timer
+    start_timer = time.time()
+    sio.emit('ping_from_client')
+
+
+@sio.event
+def connect():
+    print('connected to server')
+    send_ping()
+
+
+@sio.event
+def pong_from_server():
+    global start_timer
+    latency = time.time() - start_timer
+    print('latency is {0:.2f} ms'.format(latency * 1000))
+    sio.sleep(1)
+    send_ping()
 
 
 # --- script --- 
-sio = socketio.Client()
-
-@sio.event
-def my_event(sid, data):
-    # handle the message
-    print(data)
-
-# connect the socket
-print('... connecting Client ... ')
-
-connect_res = sio.connect('http://localhost:3000')
-print('my user id is: \t', sio.sid)
-
-# wait some time before disconnecting
-
-print('Emitting a test message... ')
-sio.emit('updatePatch', {'foo': 'bar'})
-
-time.sleep(2)
-
-sio.on('updatePatchConfirm', my_event)
-  
-
-# disconnect
-sio.disconnect(); 
+sio.connect('http://localhost:3000')
 
