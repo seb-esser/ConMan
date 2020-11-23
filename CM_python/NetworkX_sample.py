353f85d2f63d9016e7a7adf39249ef17089bcee0
Sebastian_Esser <sebastian.esser@tum.de>
2020-11-23 11:03:30 +0100

    updating sample networkX script

--- CM_python/NetworkX_sample.py
+++ CM_python/NetworkX_sample.py
@@ -5,21 +5,28 @@ import matplotlib.pyplot as plt
 
 # --- script ---
 
-G_initial = nx.Graph()
-G_updated = nx.Graph()
+G_initial = nx.DiGraph()
+G_updated = nx.DiGraph()
 
 G_initial.add_nodes_from(
 	[
-	(1, {"name": "project"}),
-	(2, {"name": "site"})
+	('p', {"name": "project"}),
+	('s', {"name": "site"})
 	]
 						 )
 
-G_initial.add_edge(1, 2)
+G_initial.add_edges_from(
+	[
+		('p', 's', {'rel': 'IfcRelAggregates',
+					'id': 'ad-23.53'}), 
+		('s', 'p', {'rel': 'IfcRelAggregates'})
+	]
+)
 
-print(list(G_initial.nodes))
-print(list(G_initial.edges))
+print(G_initial.nodes.data)
+print(G_initial.edges.data)
+print(G_initial.adj)
 
-plt.subplot(121)
-nx.draw(G_initial, with_labels=True, font_weight='bold')
+plt.plot()
+nx.draw(G_initial, with_labels=True, node_color = 'r', edge_color = 'b', font_weight='bold')
 plt.show()
