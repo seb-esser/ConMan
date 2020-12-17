56d787819fcc73b449dc57030770c7c24929ba02
Sebastian_Esser <sebastian.esser@tum.de>
2020-12-17 20:10:23 +0100

    implementing nodeDiff

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/neo4j_middleware/neo4jQueryFactory.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/neo4j_middleware/neo4jQueryFactory.py
@@ -1,9 +1,14 @@
 
 
-class neo4jGraphFactory: 
+class neo4jQueryFactory: 
 
 	def __init__(self): 
 		pass
 
+	@classmethod
+	def DiffNodes(cls, nodeIDleft, nodeIDright):
+		query_left = 'MATCH l WHERE ID(l) = {}'.format(nodeIDleft)
+		query_right = 'MATCH r WHERE ID(r) = {}'.format(nodeIDright)
+		ret_statement = 'RETURN apoc.diff.nodes(l,r)'
 
 
