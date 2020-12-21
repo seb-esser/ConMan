b2b8392867c889b6f9416848b107bbf327b7f098
Sebastian_Esser <sebastian.esser@tum.de>
2020-12-21 11:10:24 +0100

    fixing minor things

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseIFCp21.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseIFCp21.py
@@ -3,15 +3,39 @@
 import ifcopenshell
 
 """ class import """
-from .neo4j_middleware.IFCp21_neo4jMapper import IFCp21_neo4jMapper
-from .neo4j_middleware.neo4jConnector import Neo4jConnector
+from neo4j_middleware.IFCp21_neo4jMapper import IFCp21_neo4jMapper
+from neo4j_middleware.neo4jConnector import Neo4jConnector
 
 # --- Script --- 
 
 print('TestScript to map a given IFC model into a simplified Neo4j graph \n')
-model = ifcopenshell.open('sampleModel4x2.ifc')
+
+model_path = './00_sampleData/IFC_stepP21/sampleModel4x2.ifc'
+
+model = ifcopenshell.open(model_path)
 
 # --- get all rooted entities -> GUID exists ---
+
+# get entiy
+site = model.by_type('IfcSite')[0]
+pt_entity = model.by_id(43)
+
+# all sub references
+all_traverses = model.traverse(pt_entity)
+# all inverse pointers
+all_inverses = model.get_inverse(pt_entity)
+
+print('traverses')
+for traverse in all_traverses: 
+    print('\t {}'.format(traverse))
+
+print('Inverses')
+for inverse in all_inverses:
+    print('\t {}'.format(inverse))
+
+# get all entity types used in the staged IFC model
+print(model.types())
+
 objDefs = model.by_type('IfcObjectDefinition')
 propDefs = model.by_type('IfcPropertyDefinition')
 rels = model.by_type('IfcRelationship')
@@ -23,8 +47,8 @@ database.connect_driver()
 # control output to console
 print('ObjectDefinitions: ')
 for objDef in objDefs:
-    # print("\t" + str(objDef))
-    IFCp21_neo4jMapper.map_entity_to_node(database, objDef)
+    print("\t" + str(objDef))
+    # IFCp21_neo4jMapper.map_entity_to_node(database, objDef)
 
 # print('PropertyDefinitions: ')
 # for propDef in propDefs:
@@ -32,6 +56,6 @@ for objDef in objDefs:
 
 print('Relationships: ')
 for rel in rels:
-    # print("\t" + str(rel))
-    IFCp21_neo4jMapper.map_relationship_to_node(rel, database)
+    print("\t" + str(rel))
+    # IFCp21_neo4jMapper.map_relationship_to_node(rel, database)
 
