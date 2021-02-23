a0d3a3441dc937974b002224bdd5f5b57e1c5191
Sebastian_Esser <sebastian.esser@tum.de>
2021-02-23 22:46:11 +0100

    removing unused stuff

--- SrcDotNet/IFCGraphParser/ModelGraphGen.Domain/Vec2D.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModelGraphGen.Domain
-{
-    public class Vec2D
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-    }
-}
--- SrcDotNet/IFCGraphParser/ModelGraphGen.Domain/Vec3D.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ModelGraphGen.Domain
-{
-    public class Vec3D : Vec2D
-    {
-        public double Z { get; set; }
-    }
-}
