707aa74982e09a004f30d1f1ade1737e266e736d
Sebastian_Esser <sebastian.esser@tum.de>
2021-04-26 16:27:32 +0200

    testing testing testing

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseIFCp21.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseIFCp21.py
@@ -1,4 +1,3 @@
-
 """ package import """
 import ifcopenshell
 import logging
@@ -8,11 +7,9 @@ import progressbar
 
 """ class import """
 from neo4j_middleware.IFCp21_MetaGraphGenerator import IFCp21_MetaGraphGenerator
-from neo4j_middleware.neo4jGraphFactory import Neo4jGraphFactory
 from neo4j_middleware.neo4jConnector import Neo4jConnector
 
-
-# --- Script --- 
+# --- Script ---
 
 # init logging
 logging.basicConfig(filename='myapp.log', level=logging.INFO)
@@ -23,42 +20,40 @@ print('connecting to neo4j database... ')
 connector = Neo4jConnector(False, True)
 connector.connect_driver()
 
-# ToDo: automate loading of unit tests. See ticket #16 in the Gitlab repo.
-# These lines all filepaths in the directory 'dir'
-dir = './00_sampleData/IFC_stepP21/**/*.ifc' 
-paths = []
-for filepath in glob.glob(dir, recursive=True):
-    paths.append(filepath)
-print(paths)
+# # These lines all filepaths in the directory 'dir'
+# dir = './00_sampleData/IFC_stepP21/**/*.ifc'
+# paths = []
+# for filepath in glob.glob(dir, recursive=True):
+#     paths.append(filepath)
+# print(paths)
 
-#paths = ['./00_sampleData/IFC_stepP21/GeomRepresentation_01/Initial_GeomRepresentation_01.ifc' # same representation
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_01/Update_GeomRepresentation_01.ifc',  # two representations
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_02/Initial_GeomRepresentation_02.ifc',	# two representations
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_02/Update_GeomRepresentation_02.ifc',	# elevated cuboid height -> PMod
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_03/Initial_GeomRepresentation_03.ifc',	# 1 proxy as cuboid 
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_03/Update_GeomRepresentation_03.ifc',	# 1 proxy as cylinder -> mainly PMod, 
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_04/Initial_GeomRepresentation_04.ifc',	# extrudedArea
-#		 './00_sampleData/IFC_stepP21/GeomRepresentation_04/Update_GeomRepresentation_04.ifc',	# BRep -> structural Mod
-#		 './00_sampleData/IFC_stepP21/wall-column/Wall-Column.ifc', 
-#		 './00_sampleData/IFC_stepP21/wall-column/Column-Wall.ifc', 
-#		 './00_sampleData/IFC_stepP21/SleeperSample/sleeper_init.ifc', 
-#		 './00_sampleData/IFC_stepP21/SleeperSample/sleeper_updated.ifc', 
-#		 './00_sampleData/IFC_stepP21/Residential_01/residential_init.ifc', 
-#		 './00_sampleData/IFC_stepP21/Residential_01/residential_updated.ifc'
-#		 ]
+paths = ['./00_sampleData/IFC_stepP21/GeomRepresentation_01/Initial_GeomRepresentation_01.ifc',  # same representation
+         './00_sampleData/IFC_stepP21/GeomRepresentation_01/Update_GeomRepresentation_01.ifc'  # two representations
+         #		 './00_sampleData/IFC_stepP21/GeomRepresentation_02/Initial_GeomRepresentation_02.ifc',	# two representations
+         #		 './00_sampleData/IFC_stepP21/GeomRepresentation_02/Update_GeomRepresentation_02.ifc',	# elevated cuboid height -> PMod
+         #		 './00_sampleData/IFC_stepP21/GeomRepresentation_03/Initial_GeomRepresentation_03.ifc',	# 1 proxy as cuboid
+         #		 './00_sampleData/IFC_stepP21/GeomRepresentation_03/Update_GeomRepresentation_03.ifc',	# 1 proxy as cylinder -> mainly PMod,
+         #		 './00_sampleData/IFC_stepP21/GeomRepresentation_04/Initial_GeomRepresentation_04.ifc',	# extrudedArea
+         #		 './00_sampleData/IFC_stepP21/GeomRepresentation_04/Update_GeomRepresentation_04.ifc',	# BRep -> structural Mod
+         #		 './00_sampleData/IFC_stepP21/wall-column/Wall-Column.ifc',
+         #		 './00_sampleData/IFC_stepP21/wall-column/Column-Wall.ifc',
+         #		 './00_sampleData/IFC_stepP21/SleeperSample/sleeper_init.ifc',
+         #		 './00_sampleData/IFC_stepP21/SleeperSample/sleeper_updated.ifc',
+         #		 './00_sampleData/IFC_stepP21/Residential_01/residential_init.ifc',
+         #		 './00_sampleData/IFC_stepP21/Residential_01/residential_updated.ifc'
+         ]
 
-increment = 100/len(paths)
+increment = 100 / len(paths)
 percent = 0
 print('Starting to generate graphs...')
-for path in paths: 
+for path in paths:
     progressbar.printbar(percent)
     # parse model
-    graphGenerator = IFCp21_neo4jMapper(connector, path, None)
+    graphGenerator = IFCp21_MetaGraphGenerator(connector, path, None)
 
     graphGenerator.generateGraph()
 
     percent += increment
-print('\n 100% done. Graphs generated.')	
+print('\n 100% done. Graphs generated.')
 # disconnect from database
 connector.disconnect_driver()
-
