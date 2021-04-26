6b37fecfaa278a29474ee2720f85204ff360afa6
Sebastian_Esser <sebastian.esser@tum.de>
2021-04-26 14:14:57 +0200

    fixing inconsistent naming refactoring once again

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseIFCp21.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseIFCp21.py
@@ -7,7 +7,7 @@ import glob
 import progressbar
 
 """ class import """
-from neo4j_middleware.IFCp21_neo4jMapper import IFCp21_neo4jMapper
+from neo4j_middleware.IFCp21_MetaGraphGenerator import IFCp21_MetaGraphGenerator
 from neo4j_middleware.neo4jGraphFactory import neo4jGraphFactory
 from neo4j_middleware.neo4jConnector import Neo4jConnector
 
