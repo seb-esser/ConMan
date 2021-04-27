e4a1f906e2a90eba5953a896a8e093aa819a12ec
Sebastian_Esser <sebastian.esser@tum.de>
2021-04-27 11:30:56 +0200

    merging and combining some test cases

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_SimilarityDiscussion.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_SimilarityDiscussion.py
@@ -181,14 +181,46 @@ def generateModel3(connector, timestamp):
 	cy = Neo4jGraphFactory.add_attributes_by_node_id(point3_node, attrDict_point3, timestamp)
 	connector.run_cypher_statement(cy)
 
+def generateModel4(connector, timestamp):
+	cy = Neo4jGraphFactory.create_primary_node("NwVbQ3K3YQLO6GsWrQ", "ShapeElement", timestamp)
+	rootNodeId = connector.run_cypher_statement(cy, 'ID(n)')[0]
 
+	attrDict_rooted = {'p21_id': 4}
+	cy = Neo4jGraphFactory.add_attributes_by_node_id(rootNodeId, attrDict_rooted, timestamp)
+	connector.run_cypher_statement(cy)
+
+	# build line 1
+	cy = Neo4jGraphFactory.create_secondary_node(rootNodeId, "Line", "Representation", timestamp)
+	line1_node = connector.run_cypher_statement(cy, 'ID(n)')[0]
+	attrDict_line1 = {'Name': "Line 1",
+					  'p21_id': 1}
+	cy = Neo4jGraphFactory.add_attributes_by_node_id(line1_node, attrDict_line1, timestamp1)
+	connector.run_cypher_statement(cy)
+
+	# build points
+	# points of line 1
+	cy = Neo4jGraphFactory.create_secondary_node(line1_node, "Point", "StartPoint", timestamp)
+	point1_node = connector.run_cypher_statement(cy, 'ID(n)')[0]
+	attrDict_point1 = {'XCoord': 0,
+					   'YCoord': 2.0,
+					   'ZCoord': 0,
+					   'p21_id': 2}
+	cy = Neo4jGraphFactory.add_attributes_by_node_id(point1_node, attrDict_point1, timestamp)
+	connector.run_cypher_statement(cy)
+
+	cy = Neo4jGraphFactory.create_secondary_node(line1_node, "Point", "EndPoint", timestamp)
+	point2_node = connector.run_cypher_statement(cy, 'ID(n)')[0]
+	attrDict_point2 = {'XCoord': 1.0,
+					   'YCoord': 5.5,
+					   'ZCoord': 0,
+					   'p21_id': 3}
+	cy = Neo4jGraphFactory.add_attributes_by_node_id(point2_node, attrDict_point2, timestamp)
+	connector.run_cypher_statement(cy)
 # script
 
 plt.plot([0,1,7], [2, 5.5, 3], 'x-')
 plt.grid()
 #plt.show()
-
- 
 
 connector = Neo4jConnector()
 connector.connect_driver()
@@ -196,6 +228,7 @@ connector.connect_driver()
 timestamp1 = "simExplModel1"
 timestamp2 = "simExplModel2"
 timestamp3 = "simExplModel3"
+timestamp4 = "simExplModel4"
 
 # delete all
 cy = 'MATCH (n:{}) DETACH DELETE n'.format(timestamp1)
@@ -209,8 +242,7 @@ connector.run_cypher_statement(cy)
 generateModel1(connector, timestamp1)
 generateModel2(connector, timestamp2)
 generateModel3(connector, timestamp3)
-
-
+generateModel4(connector, timestamp4)
 
 connector.disconnect_driver()
 
