8eb7ab7883f46648e313c4673782077f6508754d
Sebastian_Esser <sebastian.esser@tum.de>
2021-04-29 09:26:22 +0200

    ifc generator for primary nodes

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/neo4j_middleware/NodeItem.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/neo4j_middleware/NodeItem.py
@@ -3,14 +3,14 @@ class NodeItem:
     reflects the node structure from neo4j
     """
 
-    def __init__(self, id, relType, entityType= None):
+    def __init__(self, id, relType, entityType=None):
         self.id = id
         self.entityType = entityType
         self.hash = None
         self.relType = relType
         self.attrs = None
-        
-    def setHash(self, hash): 
+
+    def setHash(self, hash):
         self.hash = hash
 
     def get_hash(self):
@@ -22,19 +22,43 @@ class NodeItem:
     @classmethod
     def fromNeo4jResponseWithRel(cls, raw):
         ret_val = []
-        for inst in raw: 
-            child = cls(inst[0], inst[1], inst[2]) 
+        for inst in raw:
+            child = cls(inst[0], inst[1], inst[2])
             ret_val.append(child)
         return ret_val
 
     @classmethod
     def fromNeo4jResponseWouRel(cls, raw):
         ret_val = []
-        for inst in raw: 
+        for inst in raw:
             child = cls(id=inst[0], relType=None, entityType=inst[1])
             ret_val.append(child)
         return ret_val
 
     def setNodeAttributes(self, attrs):
-        self.attrs = attrs
+        """
+        assigns attributes to node item
+        @param attrs: dict or list
+        @return: nothing
+        """
+        if isinstance(attrs, list):
+            d = attrs[0]
+            self.attrs = d
+        else:
+            self.attrs = attrs
 
+    def tidy_attrs(self):
+        """
+        removes entityType and p21_id from attr dict
+        @return:
+        """
+        self.attrs.pop("entityType")
+        self.attrs.pop("p21_id")
+
+        # remove attrs that have a none value assigned
+        cleared_dict = {}
+        for key, val in self.attrs.items():
+            if val != 'None':
+                cleared_dict[key] = val
+
+        self.attrs = cleared_dict
--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseGraphToIFCp21.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_parseGraphToIFCp21.py
@@ -1,17 +1,38 @@
 
 
 from ifc_middleware.IfcGenerator import IfcGenerator
+from neo4j_middleware.Neo4jQueryFactory import Neo4jQueryFactory
+from neo4j_middleware.neo4jConnector import Neo4jConnector
+from neo4j_middleware.NodeItem import NodeItem
 
+connector = Neo4jConnector(False, False)
+connector.connect_driver()
 
 generator = IfcGenerator()
-model = generator.build_file_skeleton()
+query_factory = Neo4jQueryFactory()
 
-rooted_elems = model.by_type('IfcRoot')
+ts = "ts20210119T085408"
 
-for r in rooted_elems:
-    print(r)
+# get all primary nodes
+cy = query_factory.get_primary_nodes(ts)
+raw_res = connector.run_cypher_statement(cy)
 
-model.write("test.ifc")
+# cast cypher response in a list of node items
+nodes = NodeItem.fromNeo4jResponseWouRel(raw_res)
 
+for n in nodes:
+    # query all node properties of n
+    cy = query_factory.get_node_properties_by_id(n.id)
+    raw_res = connector.run_cypher_statement(cy, "properties(n)")
+    # assign properties to node object
+    n.setNodeAttributes(raw_res)
 
+    n.tidy_attrs()
+
+    # build IFC entity
+    generator.build_entity(n.id, n.entityType, n.attrs)
+
+generator.save_model(ts)
+
+connector.disconnect_driver()
 
