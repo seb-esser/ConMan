ca35b5c3d35b9470b8eb1132f68d0d678852f542
Sebastian_Esser <sebastian.esser@tum.de>
2021-06-14 11:15:08 +0200

    refactoring and commenting

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/neo4jGraphAnalysis/PatternDetector.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/neo4jGraphAnalysis/PatternDetector.py
@@ -10,20 +10,25 @@ class PatternDetector:
         self.connector: Neo4jConnector = connector
 
     def search_if_pattern_exists(self, timestamp: str, entry_node_id: int, pattern: GraphPattern) -> bool:
+        """
+        checks if a specified GraphPattern is in the specified target graph.
+        It ignores the p21_id attribute but acknowledges GlobalIds at the moment.
+        @param timestamp: timestamp of the target graph
+        @param entry_node_id: node id where to start the pattern search
+        @param pattern: a GraphPattern object specifying a set of graphEdges
+        @return: boolean true-false
+        """
 
-        num_paths = pattern.get_number_of_paths()
+        # build cypher statement
+        cy = self.search_for_pattern(entry_node_id, pattern)
 
-        cy = ""
-
-        cy = cy + 'MATCH (en) WHERE ID(en) = {}'.format(entry_node_id)
-        cy = cy + pattern.to_cypher_query()
-        cy = cy + " RETURN ".format(entry_node_id)
-        for i in range(num_paths):
-            cy = cy + 'path{}, '.format(i)
-        cy = cy[:-2]
+        # run the cypher statement
         raw = self.connector.run_cypher_statement(cy)
+
+        # encode the result: should be either 0 or 1.
         num_results = len(raw)
 
+        # specify return
         if num_results == 0:
             return False
         elif num_results == 1:
@@ -31,5 +36,34 @@ class PatternDetector:
         else:
             print('Model: {} \t EntryNodeId: {}'.format(timestamp, entry_node_id))
             raise Exception('captured more than 1 pattern. ')
+
+    def search_for_pattern(self, entry_node_id: int, pattern: GraphPattern) -> str:
+        """
+        creates a cypher query that searches a graph for a specified graph pattern
+        @param entry_node_id:
+        @param pattern:
+        @return:
+        """
+
+        # get iterator for all paths inside the given GraphPattern object
+        num_paths = pattern.get_number_of_paths()
+
+        # init cypher command
+        cy = ""
+
+        # specify entry node in cypher statement
+        cy = cy + 'MATCH (en) WHERE ID(en) = {}'.format(entry_node_id)
+
+        # append graph pattern in cypher statement
+        cy = cy + pattern.to_cypher_query()
+
+        # specify return items
+        cy = cy + " RETURN ".format(entry_node_id)
+        for i in range(num_paths):
+            cy = cy + 'path{}, '.format(i)
+        # cut off the last ', ' items from cypher string
+        cy = cy[:-2]
+
+        return cy
 
 
