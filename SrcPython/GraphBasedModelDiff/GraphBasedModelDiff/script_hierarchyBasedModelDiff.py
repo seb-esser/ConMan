21f9a37e0500ea273889bc99a8e394559d4cff81
Sebastian_Esser <sebastian.esser@tum.de>
2021-07-12 11:49:22 +0200

    renamings and extensions for hierarchical Diff

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_hierarchyBasedModelDiff.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_hierarchyBasedModelDiff.py
@@ -1,4 +1,6 @@
+from neo4jGraphDiff.Config.Configuration import Configuration
 from neo4jGraphDiff.HierarchyPatternDiff import HierarchyPatternDiff
+from neo4jGraphDiff.SecondaryNodeDiff import DfsIsomorphismCalculator
 from neo4j_middleware.ResponseParser.NodeItem import NodeItem
 from neo4j_middleware.neo4jConnector import Neo4jConnector
 
@@ -26,8 +28,18 @@ raw_updated = connector.run_cypher_statement(
 entry_init: NodeItem = NodeItem.fromNeo4jResponseWouRel(raw_init)[0]
 entry_updated: NodeItem = NodeItem.fromNeo4jResponseWouRel(raw_updated)[0]
 
-pattern_diff = HierarchyPatternDiff(connector=connector, ts_init=ts_init, ts_updated=ts_updated)
-patterns_are_similar = pattern_diff.diffPatterns(entry_init, entry_updated)
+# calc diff on substructure
+diff_engine = DfsIsomorphismCalculator(connector=connector,
+                                       label_init=ts_init,
+                                       label_updated=ts_updated,
+                                       config=Configuration.basic_config())
 
+# run diff and get node matching
+res = diff_engine.diff_subgraphs(entry_init, entry_updated)
 
+# get conNode and related primary nodes
+print(res)
+# run subgraph diff again and consider already matched node pairs now
+
+connector.disconnect_driver()
 
