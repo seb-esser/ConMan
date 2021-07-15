dd22747a996962c853805223748e34be51f7ac4b
seb-esser <sebastian.esser@tum.de>
2021-07-15 10:19:59 +0200

    query all added and deleted nodes identified by a negative pattern approach

--- SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_hierarchyBasedModelDiff.py
+++ SrcPython/GraphBasedModelDiff/GraphBasedModelDiff/script_hierarchyBasedModelDiff.py
@@ -3,6 +3,7 @@ from neo4jGraphDiff.HierarchyPatternDiff import HierarchyPatternDiff
 from neo4jGraphDiff.SecondaryNodeDiff import DfsIsomorphismCalculator
 from neo4j_middleware.Neo4jGraphFactory import Neo4jGraphFactory
 from neo4j_middleware.Neo4jQueryFactory import Neo4jQueryFactory
+from neo4j_middleware.ResponseParser.GraphPattern import GraphPattern
 from neo4j_middleware.ResponseParser.NodeItem import NodeItem
 from neo4j_middleware.neo4jConnector import Neo4jConnector
 
@@ -12,6 +13,8 @@ connector.connect_driver()
 
 ts_init = 'ts20210623T091748'
 ts_updated = 'ts20210623T091749'
+
+connector.run_cypher_statement("Match(n)-[r:SIMILAR_TO]->(m) DELETE r")
 
 # get topmost entry nodes
 raw_init = connector.run_cypher_statement(
@@ -33,15 +36,25 @@ entry_updated: NodeItem = NodeItem.fromNeo4jResponseWouRel(raw_updated)[0]
 pDiff = HierarchyPatternDiff(connector=connector, ts_init=ts_init, ts_updated=ts_updated)
 result = pDiff.diff_subgraphs(entry_init, entry_updated)
 
+# Create SIMILAR_TO relationships to mark all nodePairs that are matched
 for p in result.nodeMatchingTable.matched_nodes:
     # print(p)
     cy = """
     MATCH (n) WHERE ID(n)={0}
     MATCH (m) WHERE ID(m)= {1}
-    MERGE (n)-[:SIMILAR_TO]->(m)
-    
+    MERGE (n)-[:SIMILAR_TO]->(m)    
     """.format(p.init_node.id, p.updated_node.id)
     connector.run_cypher_statement(cy)
+
+# Find all nodes that do not have a SIMILAR_TO relationship
+cy = """
+Match (n)-[r:SIMILAR_TO]->(m) 
+WITH collect(ID(n)) as nodeIds_init, collect(ID(m)) as nodeIds_updt
+MATCH (a:{0}) WHERE NOT ID(a) IN nodeIds_init
+MATCH (b:{1}) WHERE NOT ID(b) IN nodeIds_updt
+RETURN a, b
+""".format(ts_init, ts_updated)
+raw_res = connector.run_cypher_statement(cy)
 
 connector.disconnect_driver()
 
