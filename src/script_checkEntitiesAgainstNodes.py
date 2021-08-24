04c95a3b18386954c5c335f8880210cc3bc3c28c
seb-esser <sebastian.esser@tum.de>
2021-08-24 14:43:20 +0200

    some validation approaches

---
+++ src/script_checkEntitiesAgainstNodes.py
@@ -0,0 +1,8 @@
+import ifcopenshell
+
+
+model_init = ifcopenshell.open('./00_sampleData/IFC_stepP21/solibri_example/pretty_SolibriBuilding.ifc')
+
+for inst in model_init:
+    p21_id = inst.get_info()['id']
+    
