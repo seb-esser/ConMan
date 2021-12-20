994097910042718aecc996057e38c85f57abdc94
Benedict Harder <benedict.harder@tum.de>
2021-12-20 13:02:29 +0100

    Adds attribute parsing for CityGML parser

--- src/CityGMLGraphInterface/CityGML2GraphTranslator.py
+++ src/CityGMLGraphInterface/CityGML2GraphTranslator.py
@@ -1,19 +1,19 @@
 """ package import """
+import progressbar
+from neo4j_middleware.Neo4jGraphFactory import Neo4jGraphFactory
 import re
+import xml.etree.ElementTree as ET
 
 """ file import """
-from neo4j_middleware.Neo4jGraphFactory import Neo4jGraphFactory
-import xml.etree.ElementTree as ET
-import progressbar
+
 
 class CityGMLGraphGenerator:
-    
+
     """
     CityGML to neo4j mapper. 
     Parses a given citygml file and maps its contents to a neo4j graph
     """
     # constructor
-
 
     def __init__(self, connector, file_path):
 
@@ -25,11 +25,11 @@ class CityGMLGraphGenerator:
         my_label = my_label.replace('\n', '')
         my_label = my_label.replace(' ', '')
         self.label = my_label
-        
+
         # data for progressbar
         self.increment = 100 / len(self.root.findall(".//*"))
         self.percent = 0
-    
+
     def generateGraph(self):
         """
         parses the CityGML model into the graph database
@@ -47,11 +47,10 @@ class CityGMLGraphGenerator:
         # map the root
         root_id = self.__mapEntity(self.root, 'PrimaryNode')
         self.__buildNodes(self.root, root_id)
-        
-    
+
     def __buildNodes(self, entity, parent_id):
         # recursive method
-        
+
         # get all children (1 level below)
         children = entity.findall('./')
         if not children:
@@ -62,27 +61,48 @@ class CityGMLGraphGenerator:
 
                 # map the entity as a secondary node
                 child_id = self.__mapEntity(child, 'SecondaryNode')
-                cypher_statement = Neo4jGraphFactory.merge_on_node_ids(parent_id, child_id)
+                cypher_statement = Neo4jGraphFactory.merge_on_node_ids(
+                    parent_id, child_id)
                 self.connector.run_cypher_statement(cypher_statement)
-                
+
                 self.__buildNodes(child, child_id)
-    
-    
+
     def __mapEntity(self, entity, label):
-        
+
         # print the progressbar
         progressbar.printbar(self.percent)
+
+        # get the attribute dictionary of the entity
+        attrs = entity.attrib
         
+        # remove all the namespaces
+        key_list = list(attrs.keys())    
+        for key in key_list:
+            new_key = re.sub("[\{].*?[\}]", "", key)
+            attrs[new_key] = attrs.pop(key)
+
         # get the tag of the entity (remove the namespace)
         tag = re.sub("[\{].*?[\}]", "", entity.tag)
-        my_dict = {'EntityType': tag}
+        attrs["EntityType"] = tag
+
+        # get the text if there is one
+        # if it is a position, then convert the text into individual coord-attributes
+        if entity.text:
+            if re.sub("[\{].*?[\}]", "", entity.tag) == 'pos':
+                xyz = entity.text.split()
+                attrs["X"] = xyz[0]
+                attrs["Y"] = xyz[1]
+                attrs["Z"] = xyz[2]
+            else:
+                attrs["Text"] = entity.text
         
+
         # run cypher command
         cypher_statement = Neo4jGraphFactory.create_node_with_attr(
-            label , my_dict, self.label)
-        node_id = self.connector.run_cypher_statement(cypher_statement, 'ID(n)')[0]
-        
+            label, attrs, self.label)
+        node_id = self.connector.run_cypher_statement(
+            cypher_statement, 'ID(n)')[0]
+
         # increment the percentage and return the id
         self.percent += self.increment
         return node_id
-        
