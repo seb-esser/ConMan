f142edd18373078da13f67c0c2cb3c33b9c0cbb3
Benedict Harder <benedict.harder@tum.de>
2021-12-22 12:35:18 +0100

    adds extended filtering from nodes to attributes

--- src/CityGMLGraphInterface/CityGML2GraphTranslator.py
+++ src/CityGMLGraphInterface/CityGML2GraphTranslator.py
@@ -51,6 +51,15 @@ class CityGMLGraphGenerator:
     def __buildNodes(self, entity, parent_id):
         # recursive method
 
+        attributes = self.filterNodesToAttributes(entity)
+
+        # map the entity
+        entity_id = self.__mapEntity(entity, 'SecondaryNode', attributes)
+
+        cypher_statement = Neo4jGraphFactory.merge_on_node_ids(
+            parent_id, entity_id)
+        self.connector.run_cypher_statement(cypher_statement)
+
         # get all children (1 level below)
         children = entity.findall('./')
         if not children:
@@ -59,43 +68,42 @@ class CityGMLGraphGenerator:
             # iterate over all children
             for child in children:
 
-                # map the entity as a secondary node
-                child_id = self.__mapEntity(child, 'SecondaryNode')
-                cypher_statement = Neo4jGraphFactory.merge_on_node_ids(
-                    parent_id, child_id)
-                self.connector.run_cypher_statement(cypher_statement)
+                if not self.checkTags(child):
 
-                self.__buildNodes(child, child_id)
+                    # recursive call
+                    self.__buildNodes(child, entity_id)
 
-    def __mapEntity(self, entity, label):
+    def __mapEntity(self, entity, label, attributes=None):
 
         # print the progressbar
         progressbar.printbar(self.percent)
 
         # get the attribute dictionary of the entity
         attrs = entity.attrib
-        
+
         # remove all the namespaces
-        key_list = list(attrs.keys())    
+        key_list = list(attrs.keys())
         for key in key_list:
-            new_key = re.sub("[\{].*?[\}]", "", key)
+            new_key = self.removeNs(key)
             attrs[new_key] = attrs.pop(key)
 
         # get the tag of the entity (remove the namespace)
-        tag = re.sub("[\{].*?[\}]", "", entity.tag)
+        tag = self.removeNs(entity.tag)
         attrs["EntityType"] = tag
 
         # get the text if there is one
         # if it is a position, then convert the text into individual coord-attributes
         if entity.text:
-            if re.sub("[\{].*?[\}]", "", entity.tag) == 'pos':
+            if entity.tag == '{http://www.opengis.net/gml}pos':
                 xyz = entity.text.split()
                 attrs["X"] = xyz[0]
                 attrs["Y"] = xyz[1]
                 attrs["Z"] = xyz[2]
             else:
                 attrs["Text"] = entity.text
-        
+
+        if attributes != None:
+            attrs.update(attributes)
 
         # run cypher command
         cypher_statement = Neo4jGraphFactory.create_node_with_attr(
@@ -106,3 +114,60 @@ class CityGMLGraphGenerator:
         # increment the percentage and return the id
         self.percent += self.increment
         return node_id
+
+    def filterNodesToAttributes(self, entity):
+
+        attrs = {}
+        for child in entity.findall('./'):
+            if self.checkTags(child):
+                if self.removeNs(child.tag) == 'measureAttribute' or self.removeNs(child.tag) == 'stringAttribute':
+
+                    attr_name = child.attrib['name']
+                    child2 = child.findall('./')[0]
+
+                    if 'uom' in child2.attrib.keys():
+                        attr_name = attr_name + child2.attrib['uom']
+
+                    attrs[attr_name] = child2.text
+
+                else:  # child.text:
+                    attrs[self.removeNs(child.tag)] = child.text
+
+        return attrs
+
+    def checkTags(self, entity):
+        # check if a node should actually be an attribute of the parent node
+        # xml namespaces
+        core = '{http://www.opengis.net/citygml/2.0}'
+        bldg = '{http://www.opengis.net/citygml/building/2.0}'
+        gen = '{http://www.opengis.net/citygml/generics/2.0}'
+        grp = '{http://www.opengis.net/citygml/cityobjectgroup/2.0}'
+        app = '{http://www.opengis.net/citygml/appearance/2.0}'
+        gml = '{http://www.opengis.net/gml}'
+        xAL = '{urn:oasis:names:tc:ciq:xsdschema:xAL:2.0}'
+        xLink = '{http://www.w3.org/1999/xlink}'
+        xsi = '{http://www.w3.org/2001/XMLSchema-instance}'
+
+        # tags of nodes to be converted to attributes for the node "building"
+        building_list = ['{}description'.format(gml),
+                         '{}name'.format(gml),
+                         '{}creationDate'.format(core),
+                         '{}relativeToTerrain'.format(core),
+                         '{}class'.format(bldg),
+                         '{}function'.format(bldg),
+                         '{}usage'.format(bldg),
+                         '{}yearOfConstruction'.format(bldg),
+                         '{}roofType'.format(bldg),
+                         '{}measuredHeight'.format(bldg),
+                         '{}storeysAboveGround'.format(bldg),
+                         '{}storeysBelowGround'.format(bldg),
+                         '{}measureAttribute'.format(gen),
+                         '{}stringAttribute'.format(gen)]
+
+        if entity.tag in building_list:
+            return True
+        else:
+            return False
+
+    def removeNs(self, text):
+        return re.sub("[\{].*?[\}]", "", text)
