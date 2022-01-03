a19ac49ccf677ca8dfb726438badc0dea857f0e1
Sebastian Esser <sebastian.esser@tum.de>
2022-01-03 16:42:51 +0100

    refactoring

--- src_RevitTransactionTracker/TransactionLocalServer/run.py
+++ src_RevitTransactionTracker/TransactionLocalServer/run.py
@@ -1,4 +1,27 @@
-from server import app
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_landing_page():
+    return render_template("index.html")
+
+
+@app.route('/api/ReportTransaction', methods=["POST"])
+def report_transaction():
+    # decode request args
+
+    bdy = request.json
+    print(bdy)
+
+    # respond with a json object
+    response_json = {
+        "status": "ok"
+    }
+
+    return jsonify(response_json)
+
 
 if __name__ == "__main__":
     app.run()
