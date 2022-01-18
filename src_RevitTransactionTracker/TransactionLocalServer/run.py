2687d2d2ea4bd66beda51c70e5e0f7c0a0d32d17
Sebastian Esser <sebastian.esser@tum.de>
2022-01-18 11:31:15 +0100

    enable cross origin calls. Not recommended for production

--- src_RevitTransactionTracker/TransactionLocalServer/run.py
+++ src_RevitTransactionTracker/TransactionLocalServer/run.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, render_template, json
 from werkzeug.exceptions import HTTPException
 from flask_socketio import send, emit, SocketIO
-
+from flask_cors import CORS
 from static.globalVariables import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
+CORS(app)
 
 
 @app.errorhandler(HTTPException)
@@ -59,6 +60,16 @@ def report_transaction():
 @socketio.on('message')
 def handle_message(data):
     print('[WS] received message: ' + data)
+
+
+@socketio.event
+def connect():
+    print("[WS]: New client has connected via websocket. ")
+
+
+@socketio.event
+def disconnect():
+    print("[WS]: Existing client has disconnected via websocket. ")
 
 
 if __name__ == "__main__":
