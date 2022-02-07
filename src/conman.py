de5330b86ad08462f184d0d2650c30800658710c
Benedict Harder <benedict.harder@tum.de>
2022-02-07 13:22:05 +0100

    adds comments to conman.py

--- src/conman.py
+++ src/conman.py
@@ -13,24 +13,31 @@ import scripts.GraphBasedModelDiff as graphbaseddiff
 
 parser = argparse.ArgumentParser()
 
+# define arguments
 parser.add_argument("-i", "--input", type=str,
-                    help="Specifies the input type. If 'diff' is selected, all other args will be ignored.\nEdit diff in scripts/GraphBasedModelDiff.py", choices=["ifc", "citygml", "graph", "diff"])
+                    help="Specifies the input type. If 'diff' is selected, all other args will be ignored. Edit diff in scripts/GraphBasedModelDiff.py", choices=["ifc", "citygml", "graph", "diff"])
 parser.add_argument("-o", "--output", type=str,
                     help="Specifies the output type.", choices=["ifc", "graph"])
 parser.add_argument("-p", "--path", type=str,
-                    help="The path to the file. Can be either path to a file or a directory.\nShould a directory be given all files within that directory will be used.")
+                    help="The path to the file. Can be either path to a file or a directory. Should a directory be given all files within that directory will be used.")
 parser.add_argument("-l", "--label", type=str,
                     help="The label of the neo4j graph")
 
+# get the user arguments
 args = parser.parse_args()
 
+# check input
 if args.input == "ifc":
+    # check output
     if args.output == "graph":
+        # check if the path points to a file or directory
         if os.path.isfile(args.path):
+            # execute the corresponding script
             ifc2graph.parse([args.path])
 
         else:
             paths = []
+            # gets all .ifc files in "directory"
             directory = args.path + "/**/*.ifc"
             for filepath in glob.glob(directory, recursive=True):
                 paths.append(filepath)
@@ -39,7 +46,9 @@ if args.input == "ifc":
             ifc2graph.parse(paths)
 
     else:
+        # e.g. input "ifc" and output "ifc"
         raise Exception("Types do not match.")
+
 elif args.input == "citygml":
     if args.output == "graph":
         if os.path.isfile(args.path):
@@ -65,5 +74,6 @@ elif args.input == "graph":
     else:
         raise Exception("Types do not match.")
 
+# Execute the diff script, all other arguments are ignored in this case
 elif args.input == "diff":
     graphbaseddiff.diff()
