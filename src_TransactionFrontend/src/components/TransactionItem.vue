ce6a3b383fa488a24094d7c30612112d561a1933
Sebastian Esser <sebastian.esser@tum.de>
2022-02-07 15:46:49 +0100

    vue updates

--- src_TransactionFrontend/src/components/TransactionItem.vue
+++ src_TransactionFrontend/src/components/TransactionItem.vue
@@ -1,30 +1,23 @@
 <template>
-  <div class="p-3 mb-2">
-    <div class="row">
-      <div class="col-10">
-        <table class="table .table-sm table-bordered">
-          <thead>
-          <tr>
-            <th>ElementId</th>
-            <th>Name</th>
-            <th>State</th>
-          </tr>
-          </thead>
-          <tr>
-            <th>{{ ElementId }}</th>
-            <th>{{ ElementName }}</th>
-            <th>{{ TransactionType }}</th>
-          </tr>
-        </table>
-      </div>
-            <div class="col-2 d-grid gap-2">
-              <button class="btn btn-secondary btn-sm" @click="save">Map to IFC</button>
-
-            </div>
-
-
-    </div>
+  <div class="p-0">
+    <p align="left">
+    Id: {{ElementId}} - Name: {{ElementName}} TransactionType: {{TransactionType}} IfcGUID: {{IfcGuid}}
+    </p>
   </div>
+<!--  <div class="p-0 mb-2">-->
+<!--    <div class="row">-->
+<!--      <div class="col-10">-->
+<!--        <table class="table .table-sm table-bordered">-->
+
+<!--          <tr>-->
+<!--            <th>{{ ElementId }}</th>-->
+<!--            <th>{{ ElementName }}</th>-->
+<!--            <th>{{ TransactionType }}</th>-->
+<!--          </tr>-->
+<!--        </table>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
 
 
 </template>
@@ -33,10 +26,16 @@
 export default {
   name: "TransactionItem",
   props: {
-    ElementId: Number,
-    ElementName: String,
-    ElementUniqueId: String,
-    TransactionType: String
+    ElementId: {type: Number, default:0},
+    ElementName: {type: String, default: "unknown", required: false},
+    ElementUniqueId: {type: String, default: ""},
+    TransactionType: {type: String, default: ""},
+    IfcGuid: {type: String, default: "n/a"},
+  },
+  beforeMount: () => {
+      if(this.ElementName.length === 0){
+        this.ElementName = "unknown";
+      }
   },
 
   data() {
