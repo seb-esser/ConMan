606b14cffe1cd326eec91b5881227f5d0ec677a5
Sebastian Esser <sebastian.esser@tum.de>
2022-05-23 10:06:33 +0200

    trying to implement bootstrap-vue

--- src_eventHub/eventhub-dashboard/src/components/EventObject.vue
+++ src_eventHub/eventhub-dashboard/src/components/EventObject.vue
@@ -1,20 +1,35 @@
 <template>
   <div class="p-0">
-    <p>
-     {{eventId}} - {{topic}} - {{payload}}
-    </p>
+
+    {{ eventId }} - {{ topic }} - {{ payload }}
+    <div>
+      <b-button-toolbar key-nav aria-label="Toolbar with button groups">
+
+        <b-button-group class="mx-1">
+          <b-button>Edit</b-button>
+          <b-button>Undo</b-button>
+          <b-button>Redo</b-button>
+        </b-button-group>
+
+      </b-button-toolbar>
+    </div>
+
   </div>
 
 </template>
 
 <script>
+
 export default {
   name: "EventObject",
   props: {
     eventId: {type: Number, default: 0},
     topic: {type: String, default: ""},
-    payload: {type: String, default: ""}},
-  data() {return {} }
+    payload: {type: String, default: ""}
+  },
+  data() {
+    return {}
+  }
 }
 </script>
 
--- src_eventHub/eventhub-dashboard/src/main.js
+++ src_eventHub/eventhub-dashboard/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 createApp(App).mount('#app')
+
+App.use(BootstrapVue)
+App.use(IconsPlugin)
