4ddfa4d1b6252f22e1af5f4d3fa65abeabe97216
ge35xib <jana.hofmeyer@tum.de>
2022-06-29 14:34:31 +0200

    UniClassParser_adjustments

--- src_eventHub/eventhub-backend/UniClassParser.py
+++ src_eventHub/eventhub-backend/UniClassParser.py
@@ -9,27 +9,23 @@ with open('data_structures/SubscriptionManagement/models/Uniclass2015_Pr.csv', n
     reader = csv.DictReader(csvfile)
     #Einträge der Reihe nach einlesen
     for row in reader:
-        #print(row)
-        #print(row['Code'], row['Title'])
         test_str = row['Code']
         counter = test_str.count('_')
         #in Abhängigkeit der hierarchy structure zuordnen und einlesen
         if counter == 1:
-            group = Topic(row['Title'], row['Code'])
+            group = Topic(row['Title'])
             model.topics.extend([group])
             print(group)
         elif counter == 2:
-            sub_group = Topic(row['Title'], row['Code'])
+            sub_group = Topic(row['Title'])
             group.sub_topics.extend([sub_group])
         elif counter == 3:
-            section = Topic(row['Title'], row['Code'])
+            section = Topic(row['Title'])
             sub_group.sub_topics.extend([section])
         elif counter == 4:
-            object = Topic(row['Title'], row['Code'])
-            section.sub_topics.extend([object])
+            object_section = Topic(row['Title'])
+            section.sub_topics.extend([object_section])
         else:
             break
 
     model.to_json("")
-
-
