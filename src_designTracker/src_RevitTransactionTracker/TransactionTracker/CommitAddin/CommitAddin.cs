5e12f0c3255c4b4c20afd28dbe20c11bd9e918ad
Sebastian Esser <sebastian.esser@tum.de>
2022-09-05 11:36:34 +0200

    commit addin

---
+++ src_designTracker/src_RevitTransactionTracker/TransactionTracker/CommitAddin/CommitAddin.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CommitAddin
+{
+    [Autodesk.Revit.Attributes.Regeneration(Autodesk.Revit.Attributes.RegenerationOption.Manual)]
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class CommitAddin : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            try
+            {
+                //FilteredElementCollector coll = new FilteredElementCollector(doc).WherePasses(
+                //    new LogicalOrFilter(
+                //        new ElementIsElementTypeFilter(false),
+                //        new ElementIsElementTypeFilter(true))
+                //);
+                FilteredElementCollector coll
+                    = new FilteredElementCollector(doc)
+                        .WhereElementIsNotElementType();
+                var familyInstances = coll.OfClass(typeof(FamilyInstance));
+
+                Debug.WriteLine("count coll: " + coll.Count());
+
+                var tracking = new List<string>(); 
+
+                foreach (var element in familyInstances)
+                {
+                    // get element id
+                    var elementId = element.Id;
+
+                    // get element UniqueId
+                    var objectGuid = element.UniqueId;
+
+                    // get version id
+                    var versionGuid = element.VersionGuid;
+
+                    // get element name
+                    var name = element.Name;
+
+                    tracking.Add($"{objectGuid} \t {versionGuid} \t {name} \t {elementId}");
+
+                    
+                }
+
+                DateTime utcDate = DateTime.UtcNow;
+                string fileName = @"C:\Users\ga38hep\dev\" + utcDate.ToShortDateString().Replace(".", "-") + "_" + utcDate.ToShortTimeString().Replace(":", "-") + ".txt";
+                Debug.WriteLine(fileName);
+
+                using (StreamWriter writeText = new StreamWriter(@fileName))
+                {
+                    foreach (var t in tracking)
+                    {
+                        writeText.WriteLine(t);
+                    }
+
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Debug.WriteLine(e);
+            }
+
+            return Result.Succeeded;
+        }
+
+
+    }
+}
