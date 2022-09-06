20f2786a9aca87b4bd38850bdc1ed870d947526e
Sebastian Esser <sebastian.esser@tum.de>
2022-09-06 17:03:51 +0200

    enhanced functionality on CLI: -s for status -c for commit (either file or directory) -a -p for staging files

--- src/conman.py
+++ src/conman.py
@@ -8,33 +8,100 @@ import scripts.Ifc2Graph as ifc2graph
 import scripts.CityGML2Graph as citygml2graph
 import scripts.Graph2Ifc as graph2ifc
 import scripts.GraphBasedModelDiff as graphbaseddiff
+from scripts.GetStatus import get_status
+
+# --- Methods ---
+
+def commit():
+    all_pairs = []
+    for pair in all_pairs:
+        graphbaseddiff.diff(pair[0], pair[1])
+
 
 # --- Script ---
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser('ConMan')
 
 # define arguments
-parser.add_argument("-i", "--input", type=str,
-                    help="Specifies the input type. If 'diff' is selected, all other args will be ignored. Edit diff in scripts/GraphBasedModelDiff.py", choices=["ifc", "citygml", "graph", "diff"])
-parser.add_argument("-o", "--output", type=str,
-                    help="Specifies the output type.", choices=["ifc", "graph"])
-parser.add_argument("-p", "--path", type=str,
-                    help="The path to the file. Can be either path to a file or a directory. Should a directory be given all files within that directory will be used.")
-parser.add_argument("-l", "--label", type=str,
+# parser.add_argument("-i", "--input",
+#                     type=str,
+#                     help="Specifies the input type. If 'diff' is selected, all other args will be ignored. "
+#                          "Edit diff in scripts/GraphBasedModelDiff.py",
+#                     choices=["ifc", "citygml", "graph", "diff"])
+# parser.add_argument("-o", "--output", type=str,
+#                     help="Specifies the output type.",
+#                     choices=["ifc", "graph"])
+
+# status
+parser.add_argument("-s", "--status",
+                    action="store_true",
+                    help="reports repository status",
+                    )
+
+# add file to tracking
+parser.add_argument("-a", "--add",
+                    type=str,
+                    help="add a BIM model (or a new version, respectively) to the repository. -p is required",
+                    # choices=["ifc", "citygml", ".bim"]
+                    )
+# specify location for add function
+parser.add_argument("-p", "--path",
+                    type=str,
+                    help="The path to the file. Can be either path to a file or a directory. "
+                         "Should a directory be given, all files of the specified file type will be added.")
+
+# get file from graph
+parser.add_argument("-g", "--get",
+                    type=str,
+                    help="get a BIM model (or a new version, respectively) from the repository "
+                         "and create a file representation. -l is required. ",
+                    )
+# specify graph label for get-function
+parser.add_argument("-l", "--label",
+                    type=str,
                     help="The label of the neo4j graph")
+
+# commit
+parser.add_argument("-c", "--commit",
+                    type=str,
+                    help="generate transformation rules out of the most recent graph and the last committed "
+                         "version of a model \n")
+
+# push
+parser.add_argument("-push", "--push",
+                    type=str,
+                    help="push transformation rules to server. "
+                         "Remote settings to be defined in remoteConfig.txt \n")
+
+# fetch
+parser.add_argument("-f", "--fetch",
+                    type=str,
+                    help="fetch transformation rules from server. "
+                         "Remote settings to be defined in remoteConfig.txt \n")
+
+# fetch
+parser.add_argument("-pull", "--pull",
+                    type=str,
+                    help="apply transformation rules to local repository. "
+                         "Remote settings to be defined in remoteConfig.txt \n")
 
 # get the user arguments
 args = parser.parse_args()
 
+print(args)
+
 # check input
-if args.input == "ifc":
-    # check output
-    if args.output == "graph":
+
+if args.status is True:
+    get_status()
+
+elif args.add is not None:
+    # get path from --p flag and parse model into graph database
+    if args.add == "ifc":
         # check if the path points to a file or directory
         if os.path.isfile(args.path):
             # execute the corresponding script
             ifc2graph.parse([args.path])
-
         else:
             paths = []
             # gets all .ifc files in "directory"
@@ -45,35 +112,34 @@ if args.input == "ifc":
 
             ifc2graph.parse(paths)
 
-    else:
-        # e.g. input "ifc" and output "ifc"
-        raise Exception("Types do not match.")
+    elif args.input == "citygml":
+        if args.output == "graph":
+            if os.path.isfile(args.path):
+                citygml2graph.parse([args.path])
 
-elif args.input == "citygml":
-    if args.output == "graph":
-        if os.path.isfile(args.path):
-            citygml2graph.parse([args.path])
+            else:
+                paths = []
+                directory = args.path + "/**/*.gml"
+                for filepath in glob.glob(directory, recursive=True):
+                    paths.append(filepath)
+
+                citygml2graph.parse(paths)
 
         else:
-            paths = []
-            directory = args.path + "/**/*.gml"
-            for filepath in glob.glob(directory, recursive=True):
-                paths.append(filepath)
+            raise Exception("Types do not match.")
 
-            citygml2graph.parse(paths)
+elif args.get is not None:
+    # parse graph to file
+    label = args.label
+    if args.get == "ifc":
+        graph2ifc.parse(label)
 
-    else:
-        raise Exception("Types do not match.")
-elif args.input == "graph":
-    if args.output == "ifc":
-        graph2ifc.parse()
-
-    elif args.output == "citygml":
-        print("Not implemented yet.")
+    elif args.get == "citygml":
+        raise Exception("Not implemented yet.")
 
     else:
         raise Exception("Types do not match.")
 
-# Execute the diff script, all other arguments are ignored in this case
-elif args.input == "diff":
-    graphbaseddiff.diff()
+elif args.commit is not None:
+    # calculate diff and prepare patch
+    commit()
---
+++ src/scripts/GetStatus.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from pprint import pprint
+
+from neo4j_middleware.ResponseParser.NodeItem import NodeItem
+from neo4j_middleware.neo4jConnector import Neo4jConnector
+
+
+def get_status():
+    connector = Neo4jConnector()
+    connector.connect_driver()
+
+    cy = "MATCH (n:PrimaryNode {EntityType: \"IfcProject\"})-->(h:SecondaryNode{EntityType: \"IfcOwnerHistory\"}) " \
+         "RETURN ID(n), n.EntityType, PROPERTIES(n), LABELS(n)"
+    raw = connector.run_cypher_statement(cy)
+
+    project_nodes = NodeItem.from_neo4j_response(raw, with_rel=False)
+
+    cy = "MATCH (n:PrimaryNode {EntityType: \"IfcProject\"})-->(h:SecondaryNode{EntityType: \"IfcOwnerHistory\"}) " \
+         "RETURN ID(h), h.EntityType, PROPERTIES(h), LABELS(h)"
+    raw = connector.run_cypher_statement(cy)
+
+    history_nodes = NodeItem.from_neo4j_response(raw, with_rel=False)
+
+    zipped = zip(project_nodes, history_nodes)
+
+    for z in zipped:
+        project_guid = z[0].attrs["GlobalId"]
+        project_name = z[0].attrs["Name"]
+        created = int(z[1].attrs["CreationDate"])
+        creation_date = datetime.utcfromtimestamp(created).strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = z[0].get_timestamps()[0]
+        print("{0} \t {1} \t {2} \t {3}".format(timestamp, creation_date, project_guid, project_name))
+
+
+if __name__ == "__main__":
+    get_status()
