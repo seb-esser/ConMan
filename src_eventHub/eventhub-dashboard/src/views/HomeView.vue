83326529833d7ba97f59236721c9b0ebab781d0c
Sebastian Esser <sebastian.esser@tum.de>
2022-12-15 16:49:10 +0100

    testing IfcJS

--- src_eventHub/eventhub-dashboard/src/views/HomeView.vue
+++ src_eventHub/eventhub-dashboard/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <HelloWorld />
+  <ModelViewer />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
 
 // Components
-import HelloWorld from '../components/HelloWorld.vue';
+// import HelloWorld from '../components/HelloWorld.vue';
+import ModelViewer from '../components/ModelViewer.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    HelloWorld,
+    ModelViewer,
   },
 });
 </script>
