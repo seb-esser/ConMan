cc1250f4020bff4d40c34ffcf04cad9d429e20c7
Sebastian Esser <sebastian.esser@tum.de>
2023-07-27 15:34:53 +0200

    minorest progress entity alignment things

--- src/script_alignPartialModels.py
+++ src/script_alignPartialModels.py
@@ -1,22 +1,36 @@
 from neo4j_middleware.neo4jConnector import Neo4jConnector
 
 
-def main():
-    connector = Neo4jConnector()
-    connector.connect_driver()
-
+def align_by_guid(connector):
     ts_1 = ""
     ts_2 = ""
 
     # align based on GUIDs
     cy = """
-    MATCH (n:PrimaryNode:{}) 
-    MATCH (m:PrimaryNode:{}) WHERE m.GlobalId = n.GlobalId
-    
-    RETURN n.EntityType, n.Name,  m.EntityType,  m.Name
-    """.format(ts_1, ts_2)
-
+        MATCH (n:PrimaryNode:{}) 
+        MATCH (m:PrimaryNode:{}) WHERE m.GlobalId = n.GlobalId
+        MERGE (n)-[:ALIGNED{AlignmentType: ''ByGuid''}]-(m) 
+        """.format(ts_1, ts_2)
     connector.run_cypher_statement(cy)
+
+
+def align_by_ito_result(connector):
+    pass
+
+
+def align_by_spatial_structure(connector):
+    pass
+
+
+def main():
+    connector = Neo4jConnector()
+    connector.connect_driver()
+
+    # by guid
+    align_by_guid(connector)
+
+    # by solibri
+    align_by_ito_result(connector)
 
     connector.disconnect_driver()
 
